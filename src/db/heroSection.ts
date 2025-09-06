b26e9c988aaf6db2bd4ee5bf2868fff699b25665
Olumide <olumide.m.ojo@gmail.com>
2025-09-06 19:06:39 +0100

    perf: rephrase text

--- src/db/heroSection.ts
+++ src/db/heroSection.ts
@@ -2,5 +2,5 @@ export const focalText = {
   bigText: "Explore New Ways to Grow your Income",
   smallText:
     "Discover great opportunities to grow your income and expand your investment portfolio with Dan-Tech Investments Limited.",
-  actionButtonText: "Invest Now",
+  actionButtonText: "Start Investing",
 };

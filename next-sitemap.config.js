9c68a63d8d79e1544f7a6cf20f496282a1ef1bfd
Olumide <olumide.m.ojo@gmail.com>
2025-10-06 07:47:25 +0100

    chore: add next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteUrl: "https://dan-techinvestments.com", // Replace with your actual domain
+  generateRobotsTxt: true, // Generates robots.txt file
+  sitemapSize: 7000, // Optional: max URLs per sitemap file
+  changefreq: "daily", // Optional: default change frequency
+  priority: 0.7, // Optional: default priority
+  outDir: "public", // Default output directory
+};
